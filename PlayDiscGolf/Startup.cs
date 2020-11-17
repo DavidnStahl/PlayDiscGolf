266970a533091145fa9c651836006aa69ce93e74
David Stahl <david.n.stahl@gmail.com>
2020-11-17 11:19:59 +0100

    development pull

--- PlayDiscGolf/Startup.cs
+++ PlayDiscGolf/Startup.cs
@@ -2,31 +2,64 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PlayDiscGolf.Data;
+using PlayDiscGolf.Data.Courses;
+using PlayDiscGolf.Data.Holes;
+using PlayDiscGolf.Models.DataModels;
+using PlayDiscGolf.Services;
+using PlayDiscGolf.Services.Admin;
 
 namespace PlayDiscGolf
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+        
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var configurationSection = Configuration.GetSection("ConnectionStrings:DefaultConnection");
+            services.AddDbContext<DataBaseContext>(options => options.UseSqlServer(configurationSection.Value));
+
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+
+            })
+            .AddEntityFrameworkStores<DataBaseContext>().AddDefaultTokenProviders()
+            .AddRoles<IdentityRole>();
+
+            services.AddScoped<IAdminCourseService, AdminCourseService>();
+            services.AddScoped<IAdminNewCountryCourseService, AdminNewCountryCourseService>();
+            services.AddScoped<IAccountService, AccountService>();
+            services.AddScoped<ICourseRepository, CourseRepository>();
+            services.AddScoped<IHoleRepository,HoleRepository>();
+            
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.AddResponseCaching();
+            services.AddHttpContextAccessor();
+
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -39,11 +72,13 @@ namespace PlayDiscGolf
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseResponseCaching();
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
